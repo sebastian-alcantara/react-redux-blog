0e5bb775076be43901b4679716f4db2fa732f8c3
Sebastian Alcantara <sebastian.h.alcantara@gmail.com>
2019-10-25 05:25:30 -0300

    Initial commit

---
+++ src/actions/index.js
@@ -0,0 +1,10 @@
+import jsonPlaceholder from '../apis/jsonPlaceholder';
+
+export const fetchPosts = () => {
+  const promise = jsonPlaceholder.get('/posts');
+
+  return {
+    type: 'FETCH_POSTS',
+    payload: promise
+  }
+}

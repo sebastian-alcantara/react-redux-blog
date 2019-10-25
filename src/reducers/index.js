14974ef23dfb15fa36fb951301b175526693127b
Sebastian Alcantara <sebastian.h.alcantara@gmail.com>
2019-10-25 06:05:24 -0300

    Working on reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,5 +1,7 @@
 import { combineReducers } from 'redux';
+import postReducer from './postsReducer';
+import postsReducer from './postsReducer';
 
 export default combineReducers({
-  replaceMe: () => 'placeholder reducer'
+  posts: postsReducer
 });
